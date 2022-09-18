ac20db489ed02ac0c63dff3f9f1f3cfd00d283a9
Eusebio Simango <eusebiosimango14@gmail.com>
2022-09-18 13:48:14 +0200

    solve .env import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import "dotenv/config"
+
+// config({ path: __dirname+'/.env' });
+var test = process.env.MY_TOKEN
+console.log(test)
 
 const app = express()
+app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
 	return response.status(200).send("Hello, World!")
+})
+
+app.get('/webhooks', (request, response) => {
+	let mode = request.query["hub.mode"]
+	let challenge = request.query["hub.challenge"]
+	let token = request.query["hub.verify_token"]
+	const myToken = process.env.MY_TOKEN
+
+	if (mode && token) {
+		if(mode == "subscribe" && token == myToken) {
+			response.status(200).send(challenge)
+		} else {
+			response.status(403)
+		}
+	}
 })
 
 app.listen(3333, () => {
