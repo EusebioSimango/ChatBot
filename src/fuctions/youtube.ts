f045315a58fbdeb2e42bb241ca3da1c711dee4c9
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2023-03-25 20:45:16 +0200

    add engines to fix render

--- src/fuctions/youtube.ts
+++ src/fuctions/youtube.ts
@@ -1,5 +1,4 @@
 import YouTube from 'youtube-sr'
-import ytdl from 'ytdl-core'
 import axios from 'axios'
 import 'dotenv/config'
 
@@ -27,26 +26,6 @@ export const searchVideoOnYoutube = async (keyword: string) => {
 	}
 }
 
-const getAvailableFormat = (formats) => {
-	const firstTry = formats.filter(format => format.qualityLabel == '480p')
-	if (firstTry) 
-		return firstTry
-	console.log(`480p didn't work trying another`)
-}
-
-export const convertYTVideoToMp4 = async (url: string) => {
-	console.log('trying to convert')
-	const { formats } = await ytdl.getInfo(url)
-	const validFormats = formats.filter(format => format.mimeType.includes('mp4'))
-	// validFormats.forEach(format => {
-	//  	console.log(format.mimeType, '-' ,format.qualityLabel)
-	// })
-	const format = getAvailableFormat(validFormats)
-	const { url } = format[0]
-
-	return url
-}
-
 export const convertYTVideoToAudio = async (url: string) => {
 	const response = await axios({
 		method: 'GET',
@@ -56,11 +35,3 @@ export const convertYTVideoToAudio = async (url: string) => {
 	const { file } = response.data
 	return file
 }
-
-async function __main__ () {
-
-	convertYTVideoToMp4('https://youtube.com/watch?v=RCUW9Z3rCKE')
-	.catch(err => console.error(err))
-}
-
-__main__()
